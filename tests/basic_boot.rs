c0d3a0a33bf266491ae6668f4f7ca242466b5b40
Dave Spadea <dave@spadea.enterprises>
2019-11-17 19:04:39 -0500

    Serial, interrupts, vga buffer, panic, println, breakpoint interrupt all working.

---
+++ tests/basic_boot.rs
@@ -0,0 +1,35 @@
+// in tests/basic_boot.rs
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(kernel::test_runner)]
+
+
+use core::panic::PanicInfo;
+
+use kernel::{println, serial_print, serial_println};
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
+}
